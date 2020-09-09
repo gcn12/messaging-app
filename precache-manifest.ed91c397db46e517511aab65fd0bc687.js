91f0fd29bb6192f72e0ffe2491fb4445d07e7439
Gareth Ng <garethcng@gmail.com>
2020-09-09 11:37:26 -0400

    Updates

---
+++ precache-manifest.ed91c397db46e517511aab65fd0bc687.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a1d59fa11b6fa9b0e6c5ce627c8cb0",
+    "url": "/messaging-app/index.html"
+  },
+  {
+    "revision": "1b9290c5ce49d22ac1cd",
+    "url": "/messaging-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "608cbe4cdb36539cff7d",
+    "url": "/messaging-app/static/css/main.852f4b47.chunk.css"
+  },
+  {
+    "revision": "1b9290c5ce49d22ac1cd",
+    "url": "/messaging-app/static/js/2.b96326b5.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/messaging-app/static/js/2.b96326b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "608cbe4cdb36539cff7d",
+    "url": "/messaging-app/static/js/main.2815759c.chunk.js"
+  },
+  {
+    "revision": "124b14fe8037f422840f",
+    "url": "/messaging-app/static/js/runtime-main.9137829c.js"
+  },
+  {
+    "revision": "8c4061097f92a653c8b991ca1cc4d9cc",
+    "url": "/messaging-app/static/media/UnreadCircle.8c406109.svg"
+  }
+]);
