d3e67e7119664c5b71db847b89fec352af771a9a
Gareth Ng <garethcng@gmail.com>
2020-09-18 19:45:21 -0400

    Updates

--- precache-manifest.04e6728528a0110196ea0562f58f6983.js
+++ precache-manifest.04e6728528a0110196ea0562f58f6983.js
@@ -1,31 +1,35 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d46826fd3568956d4daf16a4453a3ed9",
+    "revision": "9dcc70a6724b35d7730dddcd3ec47cc0",
     "url": "/messaging-app/index.html"
   },
   {
-    "revision": "929b4e01fef538ac15e4",
+    "revision": "b5f88aaca4b80cf98e03",
     "url": "/messaging-app/static/css/2.3c03f26f.chunk.css"
   },
   {
-    "revision": "87cbdc18862deb4a80b6",
+    "revision": "65b860474b647077853f",
     "url": "/messaging-app/static/css/main.852f4b47.chunk.css"
   },
   {
-    "revision": "929b4e01fef538ac15e4",
-    "url": "/messaging-app/static/js/2.5ddf7ddb.chunk.js"
+    "revision": "b5f88aaca4b80cf98e03",
+    "url": "/messaging-app/static/js/2.25b53cdd.chunk.js"
   },
   {
     "revision": "d56f7d70e49a582c3cb88e84fa36367e",
-    "url": "/messaging-app/static/js/2.5ddf7ddb.chunk.js.LICENSE.txt"
+    "url": "/messaging-app/static/js/2.25b53cdd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "87cbdc18862deb4a80b6",
-    "url": "/messaging-app/static/js/main.3c212728.chunk.js"
+    "revision": "65b860474b647077853f",
+    "url": "/messaging-app/static/js/main.082a2b67.chunk.js"
   },
   {
     "revision": "124b14fe8037f422840f",
     "url": "/messaging-app/static/js/runtime-main.9137829c.js"
+  },
+  {
+    "revision": "fc51586957bd68efac86df3bd7ed0dfa",
+    "url": "/messaging-app/static/media/9.fc515869.png"
   },
   {
     "revision": "8c4061097f92a653c8b991ca1cc4d9cc",
